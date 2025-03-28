ed1d76b8c0c1025da490fe21dc4ad5b29d25f686
xHadi3 <hadiessam9@gmail.com>
2025-03-28 23:56:12 +0300

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Hadi Al zayer on 28/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
