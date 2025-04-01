b7e69a4fe66340c538d968903c561959750ecc10
xHadi3 <hadiessam9@gmail.com>
2025-04-01 14:19:16 +0300

    .

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,17 +5,43 @@
 //  Created by Hadi Al zayer on 28/09/1446 AH.
 //
 
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    
+    let startPosition = MapCameraPosition.region(
+    MKCoordinateRegion(
+        // the location where the map starts for example this is almost saudi Arabia
+        center: CLLocationCoordinate2D(latitude: 23, longitude: 45),
+        // how much to show around it
+        span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)
+    )
+    )
+    
+    @State private var locations = [Location]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        MapReader{ proxy in
+            Map(initialPosition: startPosition){
+                ForEach(locations){ location in
+                    Annotation(location.name, coordinate: location.coordinate ){
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundStyle(.red)
+                            .frame(width: 44 , height: 44)
+                            .clipShape(.circle)
+                    }
+                }
+            }
+                .onTapGesture { position in
+                    if let coordinate = proxy.convert(position, from: .local){
+                        let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
+                        locations.append(newLocation)
+                    }
+                    
+                }
         }
-        .padding()
     }
 }
 
