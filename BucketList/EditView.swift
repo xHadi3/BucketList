b8d35c09fd976fe9d3f1fcadb14cec9a965a43d9
xHadi3 <hadiessam9@gmail.com>
2025-04-01 15:49:18 +0300

    allowing the user to change the name of the annotation (marker)

---
+++ BucketList/EditView.swift
@@ -0,0 +1,18 @@
+//
+//  EditView.swift
+//  BucketList
+//
+//  Created by Hadi Al zayer on 03/10/1446 AH.
+//
+
+import SwiftUI
+
+struct EditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditView()
+}
