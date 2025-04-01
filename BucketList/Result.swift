3ec827c4ae6e7fe2d58a09027611e03c9a4940ca
xHadi3 <hadiessam9@gmail.com>
2025-04-02 00:06:44 +0300

    adding a recomendation list of places when the user adds a check mark on a specific location

--- BucketList/Result.swift
+++ BucketList/Result.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct Result: Codable {
+    let query: Query
+}
+
+struct Query: Codable {
+    let pages: [Int: Page]
+}
+
+struct Page: Codable {
+    let pageid: Int
+    let title: String
+    let terms: [String: [String]]?
+}
+
